9e906fe76dfb5c9c7523e513cb1d547f7a27a099
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-09-02 16:56:11 +0300

    cli

--- fastapi/main.py
+++ fastapi/main.py
@@ -6,3 +6,9 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get('/greet/{name}') 
+async def greet_name(name:str) -> dict:
+    return {"message": f"Hello {name}"}
+
