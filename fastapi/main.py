080e972a1ea4d0cfbc8068a7080a073e2f06288c
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-09-03 16:54:32 +0300

    cruds

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI
 
 app = FastAPI()
-
+#get - get an information
+#post - create something
+#put - update something already exist
+#delet - delet something
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get('/greet/{name}') 
-async def greet_name(name:str) -> dict:
-    return {"message": f"Hello {name}"}
 
