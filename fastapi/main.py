8824ff70555da0cd654bd02560a2c335f81ba1c0
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-09-04 20:31:53 +0300

    restapi in cruds with fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -96,3 +96,39 @@ async def get_book(book_id:int):
        detail = "book NOT found"                
                        )
 
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str 
+
+@app.patch('/book/{book_id}')
+async def update_book(book_id: int, book_update_data:BookUpdateModel):
+   for book in books:
+      if book['id'] == book_id:
+         book['title'] = book_update_data.title
+         book['author'] = book_update_data.author
+         book['publisher'] = book_update_data.publisher
+         book['page_count'] = book_update_data.page_count
+         book['language'] = book_update_data.language
+
+         return book
+
+   raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Book not found"
+    )
+
+@app.delete('/book/{book_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id:int):
+   for book in books:
+      if book['id'] == book_id:
+         books.remove(book)
+
+         return{}
+      
+         raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Book not found"
+    )
