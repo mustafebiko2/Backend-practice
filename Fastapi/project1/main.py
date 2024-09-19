f444be030e5d6d33c6eb293511e05636d750391f
mustafebiko2 <mustafe.abdi@student.moringaschool.com>
2024-09-19 18:16:56 +0300

    4

--- Fastapi/project1/main.py
+++ Fastapi/project1/main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 class post(BaseModel):
     school: str
     establish: int
+    published: bool
 
 @app.get('/home')
 def home():
@@ -17,12 +18,12 @@ from fastapi import FastAPI, Body
 
 app = FastAPI()
 
-@app.post('/createpost')
-def create_post(payload: dict = Body(...)):
-    print(payload)
-    return {
-        "new_post": f"name: {payload['name']}, phone: {payload['phone']}, email: {payload['email']}"
-    }
+# @app.post('/createpost')
+# def create_post(payload: dict = Body(...)):
+#     print(payload)
+#     return {
+#         "new_post": f"name: {payload['name']}, phone: {payload['phone']}, email: {payload['email']}"
+#     }
 
 @app.post('/school')
 def school(new_post: post):
